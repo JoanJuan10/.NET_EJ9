1e550951418fe20c3507f4548b8622d42d88d66e
JoanJuan10 <iijoanjuan10@gmail.com>
2021-01-08 09:51:11 +0100

    Initial Commit

---
+++ EJERCICIO9/EJERCICIO9/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EJERCICIO9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
